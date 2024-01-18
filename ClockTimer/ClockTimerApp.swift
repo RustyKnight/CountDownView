918892148f8487fb14dc6f2a3cd83c74e3928608
Shane Whitehead <obishiro@gmail.com>
2024-01-19 08:41:13 +1100

    Initial Commit

---
+++ ClockTimer/ClockTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockTimerApp.swift
+//  ClockTimer
+//
+//  Created by Shane Whitehead on 19/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClockTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
